6d38080fd90ee874ad518457d8f62d3df68654b0
Dinarde <dinarde@gmail.com>
2019-08-23 12:01:37 -0300

    Acertos nos Scripts FlyWay

--- siga-ex/src/main/resources/db/migration/SIGA_UTF8_V57_0__Adicionado_novas_configuracoes.sql
+++ siga-ex/src/main/resources/db/migration/SIGA_UTF8_V57_0__Adicionado_novas_configuracoes.sql
@@ -1,7 +1,1 @@
------------------------------------------------
---	SCRIPT: Adicionados novas configurações para
---  contemplar as necessidades da SEFAZ  
------------------------------------------------
-
-
-INSERT INTO CORPORATIVO.CP_TIPO_CONFIGURACAO (ID_TP_CONFIGURACAO, DSC_TP_CONFIGURACAO, ID_SIT_CONFIGURACAO) VALUES (43, 'Utilizar MobilSelecao customizado na Juntada', 2);
+insert into SIGA.EX_TIPO_MOVIMENTACAO values(66, 'Ciência');
